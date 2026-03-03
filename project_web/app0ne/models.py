df58050da3f3694eb2d5ffacf67cff24f48ce95e
sahujeevika1101-maker <sahujeevika1101@gmail.com>
2026-03-03 14:53:58 +0530

    adding media file

--- project_web/app0ne/models.py
+++ project_web/app0ne/models.py
@@ -6,5 +6,5 @@ class TableOne(models.Model):
     name = models.CharField(max_length=100)
     mobile = models.CharField(max_length=10)
     email = models.EmailField(max_length=100)
-
+    file = models.FileField(upload_to='media/', null=True, blank=True)
     
