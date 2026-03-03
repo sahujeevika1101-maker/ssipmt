95121ddeb9cba49fbb6aefc07e4357fcee8d42ad
sahujeevika1101-maker <sahujeevika1101@gmail.com>
2026-03-03 15:53:57 +0530

    to display static file

--- project_web/app0ne/views.py
+++ project_web/app0ne/views.py
@@ -2,18 +2,24 @@ from django.shortcuts import render, redirect
 from app0ne.models import TableOne
 # Create your views here.
 def home(request):
+     user_data = TableOne.objects.all()  # Fetch all data from the database
+     
      if request.method == 'POST':
           name = request.POST.get('var_name')
           mobile = request.POST.get('mobile')
           email = request.POST.get('email')
+          file = request.FILES.get('file')  # Get the uploaded file
 
           #print(f"Name: {name}, Mobile: {mobile}, Email: {email}")
 
           # Save data to the database
-          TableOne.objects.create(name=name, mobile=mobile, email=email)
+          TableOne.objects.create(name=name, mobile=mobile, email=email, file=file)
           return redirect("/")  # Redirect to the same page after saving data
-
-     return render(request, 'home.html')
+     #json
+     data_front= {
+          'data': user_data
+     }
+     return render(request, 'home.html',data_front)
 
 
         
--- project_web/project_web/urls.py
+++ project_web/project_web/urls.py
@@ -21,3 +21,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app0ne.urls')),
 ]
+
+
+#to handle the uploaded files
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
